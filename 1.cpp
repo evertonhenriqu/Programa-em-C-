622567fd35e784078a31251e540b64bfa2cf23a8
evertonhenriqu <103076461+evertonhenriqu@users.noreply.github.com>
2022-04-05 18:49:21 -0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+main(){
+	float a,b,perimetro,area;
+	
+	cout << "digite a: ";
+	cin >> a;
+	cout << "digite b: ";
+	cin >> b;
+	perimetro = a + a + b + b;
+	area = a * b;
+	
+	cout << " o perimetro eh: " << perimetro;
+	cout << "\n a area eh: " << area;
+
+    cout << float_from_ndr(perimetro, area);
+	cin >> c;
+    cout << 
+    
